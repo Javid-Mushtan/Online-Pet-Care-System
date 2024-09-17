149b62b5f5c11658375641cbdb74336e4d327ac5
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2024-09-17 12:35:10 +0530

    new start

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menu-icon").addEventListener("click", () => {
-    const menu = document.getElementById("side-menu")
-    if (menu.style.display === 'none') {
-        menu.style.display = 'flex'
-    } else {
-        menu.style.display = 'none'
-    }
-})
